6d02c667024e94fa8ddb6f50f94239b046a5b8a2
Md Manik Hossain <mdmanik1502083874@gmail.com>
2023-08-29 03:23:28 +0600

    nexted if else

---
+++ nextedifelse.php
@@ -0,0 +1,31 @@
+<?php 
+
+$number1 = 10;
+
+$number2 = 50;
+
+if ( $number1 < $mumber2   ){
+
+    if ( $number1 < $mumber2   ){
+
+        if ( $number1 < $mumber2   ){
+
+        echo "manik hossain";
+
+        }else{
+
+        echo "number  soman  nah ";
+        }
+
+    }else{
+
+    echo "number  soman  nah ";
+    }
+
+}else{
+
+echo "number  soman  nah ";
+
+}
+
+?>
