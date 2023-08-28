3d035765e9cc7da06795cd8abe651b650dd9e7a6
Md Manik Hossain <mdmanik1502083874@gmail.com>
2023-08-29 03:15:07 +0600

    and operator add

---
+++ andop.php
@@ -0,0 +1,19 @@
+<?php 
+
+$number1 = 10;
+
+$number2 = 0;
+
+$number2 = 15;
+
+if ( ($number1 < $mumber2)  || ($number1 < $mumber2)  ){
+
+echo "manik hossain";
+
+}else{
+
+echo "number  soman  nah ";
+
+}
+
+?>
