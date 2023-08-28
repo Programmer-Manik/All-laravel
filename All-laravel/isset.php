08d88d9582b236609ac5442b713139d670bc96a8
Md Manik Hossain <mdmanik1502083874@gmail.com>
2023-08-29 04:25:13 +0600

    isset add

---
+++ All-laravel/isset.php
@@ -0,0 +1,24 @@
+<?php 
+
+$var1 = 10;
+$var2 = "Hello";
+
+if (isset($var1)) {
+echo '$var1 is set.'; 
+}
+
+if (isset($var2)) {
+echo '$var2 is set.';
+}
+
+$var3 = null;
+
+if (isset($var3)) {
+echo '$var3 is set.'; 
+}
+
+if (isset($var4)) {
+echo '$var4 is set.'; 
+}
+
+?>
