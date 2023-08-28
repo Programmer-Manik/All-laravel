c6f6571142c4b6262ac93db41d9eed4db9e382b9
Md Manik Hossain <mdmanik1502083874@gmail.com>
2023-08-29 03:07:43 +0600

    ternary opertor push

---
+++ All-laravel/ternaryoperator.php
@@ -0,0 +1,21 @@
+<?php 
+
+$number1 = 10;
+
+$number2 = 50;
+
+$name = “”;
+
+if ( number1 < mumber2   ){
+
+echo 'manik hossain';
+
+}else{
+
+echo 'number  soman  nah ';
+
+}
+
+$number = ($number1 == $number1 ) ? 'manik' : ' hossain ' ;
+
+?>
