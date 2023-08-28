bf07aca6149c0a48ef5e82ee5409f1388dbd66d2
Md Manik Hossain <mdmanik1502083874@gmail.com>
2023-08-29 04:29:55 +0600

    git push function

---
+++ All-laravel/function.php
@@ -0,0 +1,11 @@
+<?php 
+
+function addNumbers($num1, $num2) {
+    $sum = $num1 + $num2;
+    return $sum;
+    }
+    
+    $result = addNumbers(5, 3); // Calling the function
+    echo "The sum is: " . $result;
+
+?>
