cfaf2d6f5d6c182e51c590f514cbaf33716939eb
Md Manik Hossain <mdmanik1502083874@gmail.com>
2023-08-29 04:46:32 +0600

    named arguments add

---
+++ All-laravel/namedA.php
@@ -0,0 +1,9 @@
+<?php 
+function exampleFunction($param1, $param2, $param3) {
+    // Function implementation
+    }
+    
+    // Using named arguments
+    exampleFunction(param1: 'value1', param2: 'value2', param3: 'value3');
+
+?>
